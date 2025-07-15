694c4d0251bb3a48ba95423fb68ada2ea618faef
devHub24 <devhub2024@gmail.com>
2025-07-15 22:09:31 +0530

    feat: Added Logging

--- src/main/java/com/sk/bookz_customer/service/CustomerServiceImp.java
+++ src/main/java/com/sk/bookz_customer/service/CustomerServiceImp.java
@@ -5,11 +5,15 @@ import com.sk.bookz_customer.dto.CustomerDto;
 import com.sk.bookz_customer.entity.Customer;
 import com.sk.bookz_customer.mapper.CustomerMapper;
 import com.sk.bookz_customer.repo.ICustomerRepo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerServiceImp implements ICustomerService {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(CustomerServiceImp.class);
 
     private final ICustomerRepo customerRepo;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
@@ -24,6 +28,7 @@ public class CustomerServiceImp implements ICustomerService {
         customerDto.setPassword(encryptPassword(customerDto.getPassword()));
         customerDto.setCustomerStatus(CustomerStatus.ACTIVE);
         Customer savedCustomer = customerRepo.save(CustomerMapper.toCustomer(customerDto));
+        LOGGER.info("New customer saved successfully:{}", savedCustomer);
         return CustomerMapper.toCustomerDto(savedCustomer);
     }
 
