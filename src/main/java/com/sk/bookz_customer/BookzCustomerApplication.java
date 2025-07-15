175b1243912bfef33a7ab3db6a0369026fe7842c
devHub24 <devhub2024@gmail.com>
2025-07-15 19:36:26 +0530

    Basic Setup

---
+++ src/main/java/com/sk/bookz_customer/BookzCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.sk.bookz_customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookzCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookzCustomerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sk/bookz_customer/BookzCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sk.bookz_customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookzCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
